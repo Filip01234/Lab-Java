48d92fc95ae85065bbb436a5224e0bf0e1d018db
Filip01234 <sacaciu.filip@yahoo.com>
2023-12-11 18:48:27 +0200

    lab8

---
+++ lab9/SwingControlDemo.java
@@ -0,0 +1,138 @@
+package com.tutorialspoint.gui;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class SwingControlDemo {
+    private JFrame mainFrame;
+    private JLabel headerLabel;
+    private JLabel statusLabel;
+    private JPanel controlPanel;
+
+    public SwingControlDemo() {
+        prepareGUI();
+    }
+
+    public static void main(String[] args) {
+        SwingControlDemo swingControlDemo = new SwingControlDemo();
+        swingControlDemo.showTextFieldDemo();
+        swingControlDemo.showCheckBoxDemo();
+        swingControlDemo.showRadioButtonDemo();
+    }
+
+    private void prepareGUI() {
+        mainFrame = new JFrame("Java Swing Examples");
+        mainFrame.setSize(400, 400);
+        mainFrame.setLayout(new GridLayout(3, 1));
+
+        mainFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent) {
+                System.exit(0);
+            }
+        });
+        headerLabel = new JLabel("", JLabel.CENTER);
+        statusLabel = new JLabel("", JLabel.CENTER);
+        statusLabel.setSize(350, 100);
+
+        controlPanel = new JPanel();
+        controlPanel.setLayout(new BoxLayout(controlPanel, BoxLayout.Y_AXIS));
+
+        mainFrame.add(headerLabel);
+        mainFrame.add(controlPanel);
+        mainFrame.add(statusLabel);
+        mainFrame.setVisible(true);
+    }
+
+    private void showTextFieldDemo() {
+        headerLabel.setText("Control in action: JTextField");
+
+        JLabel namelabel = new JLabel("Name: ", JLabel.RIGHT);
+        final JTextField userText = new JTextField(6);
+
+        JButton loginButton = new JButton("Login");
+        loginButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String data = "Username " + userText.getText();
+                statusLabel.setText(data);
+            }
+        });
+
+        JPanel textPanel = new JPanel(new FlowLayout());
+        textPanel.add(namelabel);
+        textPanel.add(userText);
+
+        controlPanel.add(textPanel);
+    }
+
+    private void showCheckBoxDemo() {
+        headerLabel.setText("Control in action: CheckBox");
+
+        final JCheckBox chkLabel = new JCheckBox();
+        chkLabel.setMnemonic(KeyEvent.VK_C);
+
+        chkLabel.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                statusLabel.setText("Label Checkbox: " +
+                        (e.getStateChange() == 1 ? "checked" : "unchecked"));
+            }
+        });
+
+        JPanel checkBoxPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        checkBoxPanel.add(new JLabel("Label: "));
+        checkBoxPanel.add(chkLabel);
+
+        controlPanel.add(checkBoxPanel);
+        mainFrame.setVisible(true);
+    }
+    private void showRadioButtonDemo() {
+        headerLabel.setText("Control in action: RadioButton");
+
+        final JRadioButton radApple = new JRadioButton();
+        final JRadioButton radMango = new JRadioButton();
+        final JRadioButton radPeer = new JRadioButton();
+
+        radApple.setMnemonic(KeyEvent.VK_C);
+        radMango.setMnemonic(KeyEvent.VK_M);
+        radPeer.setMnemonic(KeyEvent.VK_P);
+
+        radApple.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                statusLabel.setText("First RadioButton: "
+                        + (e.getStateChange() == 1 ? "checked" : "unchecked"));
+            }
+        });
+        radMango.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                statusLabel.setText("Second RadioButton: "
+                        + (e.getStateChange() == 1 ? "checked" : "unchecked"));
+            }
+        });
+        radPeer.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                statusLabel.setText("Third RadioButton: "
+                        + (e.getStateChange() == 1 ? "checked" : "unchecked"));
+            }
+        });
+        JPanel checkBoxPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        checkBoxPanel.add(new JLabel("Label: "));
+
+
+        // Group the radio buttons.
+        ButtonGroup group = new ButtonGroup();
+        group.add(radApple);
+        group.add(radMango);
+        group.add(radPeer);
+
+        JPanel radioPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        radioPanel.add(new JLabel("Label: "));
+        radioPanel.add(radApple);
+        radioPanel.add(radMango);
+        radioPanel.add(radPeer);
+
+        controlPanel.add(radioPanel);
+
+        mainFrame.setVisible(true);
+    }
+}
+
